78d993b8cbf02e1ead6a1b89c25eb2d72bc53b2f
mdsymoon <mdsymoon11@gmail.com>
2021-09-21 11:17:34 -0700

    foodie-client

---
+++ src/Components/HomePage/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import Navigation from './../Navigation/Navigation';
+
+const Header = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+        </div>
+    );
+};
+
+export default Header;
